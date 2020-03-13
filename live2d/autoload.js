070df1d6cf63fbb317171cf82e50a0f53766df60
x0620x <x0620x@qq.com>
2020-03-13 14:43:08 +0800

    one

---
+++ live2d/autoload.js
@@ -0,0 +1,4 @@
+// 注意：live2d_path 参数应使用绝对路径
+const live2d_path = "https://cdn.jsdelivr.net/gh/x0620x/live2djs/live2d/";function loadExternalResource(url, type) {return new Promise((resolve, reject) => {let tag;if (type === "css") {tag = document.createElement("link");tag.rel = "stylesheet";tag.href = url;} else if (type === "js") {tag = document.createElement("script");tag.src = url;}if (tag) {tag.onload = () => resolve(url);tag.onerror = () => reject(url);
+document.head.appendChild(tag);}});}if (screen.width >= 768) {Promise.all([loadExternalResource(live2d_path + "waifu.css", "css"), loadExternalResource(live2d_path + "live2d.min.js", "js"), loadExternalResource(live2d_path + "waifu-tips.js", "js")]).then(() => {initWidget({waifuPath: live2d_path + "waifu-tips.json", apiPath: "https://live2d.fghrsh.net/api/", //cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+});});}
